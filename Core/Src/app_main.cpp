7bf565a2cf3eae84ea747e8a8aaf1b8275ed254c
Lieven Merckx <lieven.merckx@ing.com>
2021-11-16 18:26:53 +0100

    UART driver

--- Core/Src/app_main.cpp
+++ Core/Src/app_main.cpp
@@ -19,12 +19,14 @@ extern "C" DMA_HandleTypeDef hdma_usart2_tx;
 extern "C" IWDG_HandleTypeDef hiwdg;
 extern "C" CRC_HandleTypeDef hcrc;
 
-struct Led {
-		GPIO_TypeDef *port;
-		uint32_t pin;
-		void toggle() {
-			HAL_GPIO_TogglePin(port, pin);
-		}
+struct Led
+{
+	GPIO_TypeDef *port;
+	uint32_t pin;
+	void toggle()
+	{
+		HAL_GPIO_TogglePin(port, pin);
+	}
 };
 
 //================================================ UART
@@ -32,77 +34,16 @@ typedef std::vector<uint8_t> Bytes;
 uint8_t dmaRxBuffer[100];
 uint32_t count;
 
-/* This function handles DMA1 stream6 global interrupt. */
-extern "C" void DMA1_Stream6_IRQHandler(void) {
-	HAL_DMA_IRQHandler(&hdma_usart2_tx);
-}
-
-/*
- void usart2_rx_check(void)
- {
- static uint32_t old_pos;
- uint32_t pos;
- pos = rx_buffer_L_len - __HAL_DMA_GET_COUNTER(huart2.hdmarx);         // Calculate current position in buffer
-
- if (pos != old_pos) {                                                 // Check change in received data
- if (pos > old_pos) {                                                // "Linear" buffer mode: check if current position is over previous one
- usart_process_debug(&rx_buffer_L[old_pos], pos - old_pos);        // Process data
- } else {                                                            // "Overflow" buffer mode
- usart_process_debug(&rx_buffer_L[old_pos], rx_buffer_L_len - old_pos); // First Process data from the end of buffer
- if (pos > 0) {                                                    // Check and continue with beginning of buffer
- usart_process_debug(&rx_buffer_L[0], pos);                      // Process remaining data
- }
- }
- }
- }
- */
-
 //==================================================== UART END
-typedef enum {
-	B_PUBLISH,   // RXD,TXD id , Bytes value
+typedef enum
+{
+	B_PUBLISH,	 // RXD,TXD id , Bytes value
 	B_SUBSCRIBE, // TXD id, string, qos,
 	B_UNSUBSCRIBE,
 	B_NODE,
 	B_LOG
 } MsgType;
-#ifdef JSON
-#include <ArduinoJson.h>
 
-StaticJsonDocument<100> txDoc;
-class BytesWrite: public Bytes {
-public:
-	bool write(uint8_t b) {
-		push_back(b);
-		return true;
-	}
-	bool write(const uint8_t*& bs,size_t sz){
-		for (size_t i=0;i<sz;i++) push_back(bs[i]);
-		return true;
-	}
-} txBytes;
-
-Bytes nodeMessage(const char* name){
-	txDoc.clear();
-	txBytes.clear();
-	txDoc[0] = B_NODE;
-	txDoc[1] = name;
-	serializeJson(txDoc, txBytes);
-	return ppp_frame(txBytes);
-}
-
-Bytes publishMessage(const char* topic, uint64_t i) {
-	txDoc.clear();
-		txBytes.clear();
-		txDoc[0] = B_PUBLISH;
-		txDoc[1] = topic;
-		txDoc[2] = i;
-		serializeJson(txDoc, txBytes);
-		return ppp_frame(txBytes);
-}
-#endif
-
-#define CBOR
-#ifdef CBOR
 #include "CborWriter.h"
 #include "CborReader.h"
 #include "frame.h"
@@ -110,48 +51,155 @@ CborWriter tx(200);
 CborReader rx(10);
 uint8_t rxBuffer[100];
 
-void sendNode(const char *topic) {
-	if (tx.reset().array().add(B_NODE).add(topic).close().ok()) {
-		tx.addCrc();
-		Bytes buffer = frame(tx.bytes());
-		if (HAL_UART_Transmit_DMA(&huart2, buffer.data(), buffer.size())
-				!= HAL_OK) Log::txBufferOverflow++;
-	}
+// https://stackoverflow.com/questions/43298708/stm32-implementing-uart-in-dma-mode
+
+typedef void (*RxdFunction)(Bytes &);
+
+/* This function handles DMA1 stream6 global interrupt. */
+extern "C" void DMA1_Stream6_IRQHandler(void)
+{
+	HAL_DMA_IRQHandler(&hdma_usart2_tx);
 }
 
-template<typename T>
-void publish(const char *topic, T v) {
-	if (tx.reset().array().add(B_PUBLISH).add(topic).add(v).close().ok()) {
-		tx.addCrc();
-		Bytes buffer = frame(tx.bytes());
-		if (HAL_UART_Transmit_DMA(&huart2, buffer.data(), buffer.size())
-				!= HAL_OK) Log::txBufferOverflow++;
-	}
+void UART_IDLECallback(UART_HandleTypeDef *huart)
+{
+	__NOP();
 }
 
-#endif
+extern "C" void USART2_IRQHandler(void)
+{
+	if (__HAL_UART_GET_FLAG(&huart2, UART_FLAG_IDLE) != RESET)
+	{
+		__HAL_UART_CLEAR_IDLEFLAG(&huart2);
+		UART_IDLECallback(&huart2);
+	}
+	HAL_UART_IRQHandler(&huart2);
+}
+
+extern "C" void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	HAL_UART_Receive_DMA(huart, rxBuffer, sizeof(rxBuffer)); // next rxd cycle
+}
+
+extern "C" void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart)
+{
+	__NOP();
+}
+
+#define FRAME_MAX 100
+class Uart : public Actor
+{
+	uint32_t _txdOverflow = 0;
+	UART_HandleTypeDef *_huart;
+	uint8_t _raw[FRAME_MAX];
+	Bytes _frameRxd;
+	bool escFlag = false;
+	QueueFlow<Bytes> _framesRxd;
+	size_t _wrPtr, _rdPtr;
+
+public:
+	Uart(Thread &thread, UART_HandleTypeDef *huart) : Actor(thread), _huart(huart)
+	{
+		_framesRxd.async(thread);
+	};
+	bool init()
+	{
+		__HAL_UART_ENABLE_IT(huart, UART_IT_IDLE);
+		HAL_UART_Receive_DMA(huart, _raw, sizeof(_raw));
+	}
+	bool sendFrame(Bytes &bs)
+	{
+		Bytes buffer = frame(tx.bytes());
+		if (HAL_UART_Transmit_DMA(&huart2, buffer.data(), buffer.size()) != HAL_OK)
+			_txdOverflow++;
+	}
+	void onFrame(RxdFunction func, void *instance);
+	void UART_Data_Process(UART_HandleTypeDef *huart)
+	{
+		_wrPtr = sizeof(_raw) - huart->hdmarx->Instance->CNDTR;
+		if (_wrPtr != _rdPtr)
+		{
+			if (_wrPtr > _rdPtr)
+			{
+				for (int i = _rdPtr; i < _wrPtr; i++)
+					add(_raw[i]);
+			}
+			else
+			{
+				for (int i = _rdPtr; i < sizeof(_raw); i++)
+					add(_raw[i]);
+				if (_wrPtr > 0)
+					for (int i = 0; i < _wrPtr; i++)
+						add(_raw[i]);
+			}
+			rdPtr = wrPtr;
+		}
+	}
+	void add(uint8_t c)
+	{
+		if (c == PPP_ESC_CHAR)
+		{
+			escFlag = true;
+		}
+		else if (c == PPP_FLAG_CHAR)
+		{
+			if (_frameRxd.size())
+				_framesRxd.on(_frameRxd);
+			_frameRxd.clear();
+		}
+		else if (escFlag)
+		{
+			_frameRxd.push_back(c ^ PPP_MASK_CHAR);
+			escFlag = false;
+		}
+		else
+		{
+			_frameRxd.push_back(c);
+		}
+	}
+	Source<Bytes> &incoming() { return _framesRxd; };
+	Sink<Bytes> &outgoing(){};
+	uint32_t errors() { return _txdOverflow; }
+} uart2(&huart2);
+
+void sendNode(const char *topic)
+{
+	if (tx.reset().array().add(B_NODE).add(topic).close().addCrc().ok())
+		uart2.sendFrame(tx.bytes());
+}
+
+template <typename T>
+void publish(const char *topic, T v)
+{
+	if (tx.reset().array().add(B_PUBLISH).add(topic).add(v).close().ok())
+		uart2.sendFrame(tx.bytes());
+}
+
 static volatile bool crcDMAdone;
 
-void DMADoneCallback(DMA_HandleTypeDef *handle) {
+void DMADoneCallback(DMA_HandleTypeDef *handle)
+{
 	crcDMAdone = true;
 }
 
 Thread workerThread("worker");
 Thread spineThread("spine");
 
-class Blinker: public Actor {
-		TimerSource ticker;
-		Led yellow = { GPIOB, GPIO_PIN_1 };
-		uint32_t cnt = 0;
+class Blinker : public Actor
+{
+	TimerSource ticker;
+	Led led = {GPIOB, GPIO_PIN_1};
+	uint32_t cnt = 0;
 
-	public:
-		Blinker(Thread &thr)
-				:
-				Actor(thr), ticker(thr, 100, true, "ticker") {
-			ticker >> [&](const TimerMsg&) {
-				yellow.toggle();
-			};
-		}
+public:
+	Blinker(Thread &thr)
+		: Actor(thr), ticker(thr, 100, true, "ticker")
+	{
+		ticker >> [&](const TimerMsg &)
+		{
+			led.toggle();
+		};
+	}
 } blinker(workerThread);
 
 uint32_t wrPtr;
@@ -162,93 +210,60 @@ uint8_t RxBuf3[100];
 #include "CircBuf.h"
 CircBuf rxd(100);
 
-void UART_Data_Process(UART_HandleTypeDef *huart) {
-	wrPtr = sizeof(rxBuffer) - huart->hdmarx->Instance->CNDTR;
-	if (wrPtr != rdPtr) {
-		memset(RxBuf3, 0, MAX_RX_BUF + 16);
+class Spine : public Actor
+{
+	TimerSource ticker;
+	uint32_t count = 0;
 
-		if (wrPtr > rdPtr) {
-			rcvdLen = wrPtr - rdPtr;
-			memcpy(RxBuf3, rxBuffer + rdPtr, rcvdLen);
-		} else {
-			rcvdLen = sizeof(rxBuffer) - rdPtr;
-			memcpy(RxBuf3, rxBuffer + rdPtr, rcvdLen);
-			if (wrPtr > 0) {
-				rcvdLen += wrPtr;
-				memcpy(RxBuf3 + rcvdLen, rxBuffer, wrPtr);
+public:
+	Spine(Thread &thread)
+		: Actor(thread), ticker(thread, 100, true, "ticker")
+	{
+		INFO("Build %s : %s", __DATE__, __TIME__);
+		__HAL_UART_ENABLE_IT(&huart2, UART_IT_IDLE);
+
+		ticker >> [&](const TimerMsg &)
+		{
+			HAL_UART_Receive_DMA(&huart2, rxBuffer, sizeof(rxBuffer));
+
+			count++;
+			switch (count % 5)
+			{
+			case 0:
+			{
+				sendNode("stm32f103");
+				break;
 			}
-		}
-		rdPtr = wrPtr;
+			case 1:
+			{
+				publish("src/stm32f103/system/uptime", Sys::millis());
+				break;
+			}
+			case 2:
+			{
+				publish("dst/stm32f103/system/loopback", Sys::millis());
+				break;
+			}
+			case 3:
+			{
+				publish("src/stm32f103/system/highWaterMark", uxTaskGetStackHighWaterMark(NULL));
+				break;
+			}
+			case 4:
+			{
+				publish("src/stm32f103/system/build", __DATE__ " " __TIME__);
+				break;
+			}
+			}
+		};
 	}
-}
-
-void UART_IDLECallback(UART_HandleTypeDef *huart) {
-	__NOP();
-}
-
-void USART2_IRQHandler(void) {
-	if (__HAL_UART_GET_FLAG(&huart2, UART_FLAG_IDLE) != RESET) {
-		__HAL_UART_CLEAR_IDLEFLAG(&huart2);
-		UART_IDLECallback(&huart2);
-	}
-	HAL_UART_IRQHandler(&huart2);
-}
-
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-	__NOP();
-}
-
-void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart) {
-	__NOP();
-}
-
-class Spine: public Actor {
-		TimerSource ticker;
-		uint32_t count = 0;
-	public:
-		Spine(Thread &thread)
-				:
-				Actor(thread), ticker(thread, 100, true, "ticker") {
-			INFO("Build %s : %s", __DATE__, __TIME__);
-			__HAL_UART_ENABLE_IT(&huart2, UART_IT_IDLE);
-
-			ticker
-					>> [&](const TimerMsg&) {
-						HAL_UART_Receive_DMA(&huart2, rxBuffer, sizeof(rxBuffer));
-
-						count++;
-						switch (count % 5) {
-							case 0: {
-								sendNode("stm32f103");
-								break;
-							}
-							case 1: {
-								publish("src/stm32f103/system/uptime", Sys::millis());
-								break;
-							}
-							case 2: {
-								publish("dst/stm32f103/system/loopback", Sys::millis());
-								break;
-							}
-							case 3: {
-								publish("src/stm32f103/system/highWaterMark", uxTaskGetStackHighWaterMark( NULL));
-								break;
-							}
-							case 4: {
-								publish("src/stm32f103/system/build", __DATE__ " " __TIME__);
-								break;
-							}
-						}
-					};
-		}
-
 };
 
-extern "C" void app_main() {
+extern "C" void app_main()
+{
 	Spine spine(spineThread);
 	workerThread.start();
 	spineThread.start();
-//	xPortStartScheduler();
+	//	xPortStartScheduler();
 	osKernelStart();
 }
-
